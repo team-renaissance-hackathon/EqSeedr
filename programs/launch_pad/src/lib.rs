8b271f68c82255e9f406a3dd7eacf50bc656d02e
nomadic4life <orbison.dennis@gmail.com>
2024-03-13 10:58:45 -0700

    Implemented SessionIndexer, used for the active session indexer and enqueue session indexer, which as been added the the intializer. created logical seperations of code, by logic, validations, states, types, accounts, and constaints. left a not on the indexer.

--- programs/launch_pad/src/lib.rs
+++ programs/launch_pad/src/lib.rs
@@ -1,10 +1,12 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::pubkey::PUBKEY_BYTES};
 
 declare_id!("7GKWqKvkev22SYs2HEb1jw6h4uHJwLVKpEcxVUqTZKxG");
 
 #[program]
 pub mod launch_pad {
     use super::*;
+
+    // LOGIC
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let Initialize { new_authority, .. } = ctx.accounts;
@@ -19,6 +21,10 @@ pub mod launch_pad {
         Ok(())
     }
 }
+
+// UTILS
+
+// VALIDATIONS
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
@@ -38,23 +44,82 @@ pub struct Initialize<'info> {
     )]
     pub new_authority: Account<'info, ProgramAuthority>,
 
+    #[account(
+        init,
+        payer = authority,
+        space = SessionIndexer::LEN,
+        seeds = [
+            b"active-session-indexer",
+            new_authority.key().as_ref(),
+        ],
+        bump
+    )]
+    pub new_active_session_indexer: Account<'info, SessionIndexer>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = SessionIndexer::LEN,
+        seeds = [
+            b"enqueue-session-indexer",
+            new_authority.key().as_ref(),
+        ],
+        bump
+    )]
+    pub new_enqueue_session_indexer: Account<'info, SessionIndexer>,
+
     pub system_program: Program<'info, System>,
 }
 
+// CONSTANTS
+
+const DISCRIMINATOR: usize = 8;
 const SIGNED_64: usize = 8;
 // const UNSIGNED_128: usize = 16;
-const UNSIGNED_64: usize = 8;
+// const UNSIGNED_64: usize = 8;
 // const UNSIGNED_32: usize = 4;
 const UNSIGNED_16: usize = 2;
 const UNSIGNED_8: usize = 1;
 const BOOL: usize = 1;
+const BUMP: usize = 1;
+
+// ACCOUNTS
 
 #[account]
 pub struct ProgramAuthority {
     pub is_initialzied: bool,
     pub is_signer: bool,
+
+    // should this be in it's own state account?
+    // performance can be impacted if a lot of writes are happening,
+    // even when other actions don't require the Indexer.
+    // because this is the top level account. will update later.
     pub indexer: Indexer,
 }
+
+// #[account]
+// pub struct IndexTracker {}
+
+#[account]
+pub struct SessionIndexer {
+    pub bump: u8,
+    pub authority: Pubkey,
+
+    // not sure this will work in anchor
+    pub list: Vec<(Indexer, Pubkey)>,
+}
+
+impl ProgramAuthority {
+    const LEN: usize = DISCRIMINATOR + BUMP + BOOL + BOOL + Indexer::LEN;
+}
+
+impl SessionIndexer {
+    const LIMIT: usize = 100;
+    const LEN: usize =
+        DISCRIMINATOR + BUMP + PUBKEY_BYTES + (Indexer::LEN + PUBKEY_BYTES) * SessionIndexer::LIMIT;
+}
+
+// TYPES
 
 #[derive(AnchorDeserialize, AnchorSerialize, Clone)]
 pub struct Indexer {
@@ -72,7 +137,6 @@ impl Indexer {
         SIGNED_64 + SIGNED_64 + SIGNED_64 + UNSIGNED_16 + UNSIGNED_8 + UNSIGNED_8 + UNSIGNED_8;
 
     // will work up to year 2038, this needs an update when a new standard exist.
-    #[warn(dead_code)]
     const YEAR_DELTA: [i64; 4] = [31_622_400, 31_536_000, 31_536_000, 31_536_000];
     const WEEK_DELTA: i64 = 604_800;
     const DAY_DELTA: i64 = 86_400;
@@ -118,8 +182,4 @@ impl Indexer {
 
         self.nonce += 1;
     }
-}
-
-impl ProgramAuthority {
-    const LEN: usize = UNSIGNED_64 + BOOL + BOOL + Indexer::LEN;
 }
