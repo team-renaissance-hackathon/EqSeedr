f5a8ec2b68f764633f17e7e929fdb7da73fe14e5
nomadic4life <orbison.dennis@gmail.com>
2024-03-17 02:56:34 -0700

    validation created for CreateSealedBidRound, needs handler logic

---
+++ programs/launch_pad/src/instructions/create_sealed_bid_round.rs
@@ -0,0 +1,38 @@
+#[derive(Accounts)]
+pub struct CreateSealedBidRound<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = MarketMakerPool::LEN,
+        seeds = [
+            session.key().as_ref(),
+            b"sealed-bid-round",
+        ],
+        bump
+    )]
+    pub new_sealed_bid: Account<'info, MarketMakerPool>,
+
+    #[account(
+        mut,
+        has_one = authority
+        constraint = !session.has_marketplace @ ProgramError::SessionSealedBidRoundAlreadyExist
+    )]
+    pub session: Account<'info, Session>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<CreateSealedBidRound>) -> Result<()> {
+    let CreateSealedBidRound {
+        new_sealed_bid_round,
+        session,
+        ..
+    } = ctx.accounts;
+
+    // need set state logic
+
+    Ok(())
+}
