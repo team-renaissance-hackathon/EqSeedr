f65695ce30a17374ac2639f1ba5b450046f5507c
nomadic4life <orbison.dennis@gmail.com>
2024-03-18 21:21:51 -0700

    implemented validations for submit sealed bid

---
+++ programs/launch_pad/src/instructions/submit_sealed_bid.rs
@@ -0,0 +1,84 @@
+#[derive(Accounts)]
+pub struct SubmitSealedBid<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = SealedBidByIndex::LEN,
+        seeds = [
+            sealed_bid_round.next_index().as_ref(),
+            session.key().as_ref(),
+            b"sealed-bid-by-index",
+        ],
+        bump
+    )]
+    pub new_sealed_bid_by_index: Account<'info, SealedBidByIndex>,
+
+    #[account(
+        mut,
+        constraint = sealed_bid_round.authority == session.key(),
+    )]
+    pub sealed_bid_round: Account<'info, SealedBidRound>,
+
+    #[account(
+        mut
+        constraint = bidder_token_account.owner == authority.key()
+    )]
+    pub bidder_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut
+        constraint = session.is_valid_staking_account(session_stake_token_account.key())
+    )]
+    pub session_stake_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        constraint = token_mint.mint_authority == program_authority.key(),
+    )]
+    pub token_mint: Account<'info, Mint>,
+
+    pub program_authority: Account<'info, ProgramAuthority>,
+    pub session: Account<'info, Session>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<SubmitSealedBid>, commit_hash: Pubkey) -> Result<()> {
+    let SubmitSealedBid {
+        authority,
+        new_sealed_bid_by_index,
+        sealed_bid_round,
+        session,
+        bidder_token_account,
+        session_stake_token_account,
+        token_program,
+        ..
+    } = ctx.accounts;
+
+    new_sealed_bid_by_index.initialize(
+        ctx.bumps.new_sealed_bid_by_index,
+        sealed_bid_round.get_index(),
+        session.key(),
+        authority.key(),
+        session.staking_amount,
+        commit_hash,
+    );
+
+    sealed_bid_round.update_total_sealed_bids();
+
+    transfer(
+        CpiContext::new(
+            token_program.to_account_info(),
+            Transfer {
+                from: bidder_token_account.to_account_info(),
+                to: session_stake_token_account.to_account_info(),
+                authority: authority.to_account_info(),
+            },
+        ),
+        session.staking_amount,
+    )?;
+
+    Ok(())
+}
--- programs/launch_pad/src/states/sealed_bid_round.rs
+++ programs/launch_pad/src/states/sealed_bid_round.rs
@@ -10,7 +10,7 @@ pub struct SealedBidRound {
     pub total_unsealed_bids: u32,
 }
 
-impl TickBidRound {
+impl SealedBidRound {
     pub const LEN: usize = DISCRIMINATOR
         + UNSIGNED_8
         + PUBKEY_BYTES
@@ -31,6 +31,180 @@ impl TickBidRound {
 
         // emit event
     }
+
+    pub fn next_index(&self) -> String {
+        return (self.total_sealed_bids + 1).to_string();
+    }
+
+    pub fn get_index(&self) -> u32 {
+        return self.total_sealed_bids + 1;
+    }
+
+    pub fn update_total_sealed_bids(&self) {
+        self.total_sealed_bids += 1;
+    }
+
+    pub fn update_total_unsealed_bids(&self) {
+        self.total_unsealed_bids += 1;
+    }
+
+    pub fn is_valid_stake_amount(&self) -> bool {
+        return true;
+    }
+
+    pub fn is_valid_sealed_bid_phase(&self) -> bool {
+        return true;
+    }
+
+    pub fn is_valid_unsealed_bid_phase(&self) -> bool {
+        return true;
+    }
+
+    pub fn is_valid_unsealed_bid(&self) -> bool {
+        // take amount, hash the amount, compare the hashed amount on state
+        return true;
+    }
+
+    // don't think this is necessary, will combine the commit bid phase with unsealed bid phase
+    pub fn is_valid_commit_bid_phase(&self) -> bool {
+        return true;
+    }
+
+    pub fn is_valid_commit_amount(&self) -> bool {
+        // check user balance
+        return true;
+    }
+
+    pub fn has_not_commit(&self) -> bool {
+        return true;
+    }
+
+    pub fn is_valid_sealed_bid_round(&self) -> bool {
+        // use unix timestamp || status
+        return true;
+    }
+
+    pub fn is_valid_unsealed_bid(&self) -> bool {
+        // also check account is set as unsealed state
+        return self.total_unsealed_bids < self.total_sealed_bids;
+    }
+
+    // pub fn is_valid() {}
+}
+
+#[account]
+pub struct SealedBidByIndex {
+    // VALIDATION STATE
+    pub bump: u8,
+    pub index: u32,
+    pub session: Pubkey,
+    pub owner: Pubkey,
+
+    // STATE
+    pub commit_hash: Pubkey, // technially a hash [u8; 32]
+    pub staked_amount: u64,
+    pub is_unsealed: bool,
+}
+
+impl SealedBidByIndex {
+    pub fn initialize(
+        &mut self,
+        bump: u8,
+        index: u32,
+        session: Pubkey,
+        owner: Pubkey,
+        amount: u64,
+        commit_hash: Pubkey,
+    ) {
+        self.bump = bump;
+        self.index = index;
+        self.session = session;
+        self.owner = owner;
+
+        self.commit_hash = commit_hash;
+        self.staked_amount = amount;
+
+        self.is_unsealed = false;
+
+        // emit event
+    }
+}
+
+#[account]
+pub struct CommitLeaderBoard {
+    pub bump: u8,
+    pub session: Pubkey,
+    pub min_target: u64, // cutoff / bottom amount, increaese when commit queue has 10 -> I don't think I need this
+    pub pool: LinkedList<Commit>,
+}
+
+impl Len for CommitLeaderBoard {
+    const LEN: usize =
+        DISCRIMINATOR + BUMP + PUBKEY_BYTES + UNSIGNED_64 + LinkedList::<Commit>::LEN;
+}
+
+#[account]
+pub struct CommitQueue {
+    pub bump: u8,
+    pub session: Pubkey,
+    pointer: u8,
+    queue: Vec<Commit>,
+}
+
+const MAX_CAPACITY: usize = 10;
+impl CommitQueue {
+    const LEN: usize =
+        DISCRIMINATOR + BUMP + PUBKEY_BYTES + BYTE + (UNSIGNED_128 + (Commit::LEN * MAX_CAPACITY));
+
+    pub fn initialize(&mut self, bump: u8, session: Pubkey) {
+        self.bump = bump;
+        self.session = session;
+        self.queue = Vec::new();
+
+        // emit event
+    }
+
+    pub fn insert(&mut self, commit: Commit) {
+        let mut index = self.queue.len();
+
+        while index != 0 && commit.amount > self.queue[index - 1].unwrap().amount {
+            index -= 1;
+        }
+
+        if self.queue.len() != 0 && self.queue.len() == MAX_CAPACITY {
+            self.queue.insert(index, commit).pop();
+        } else if self.queue.len() != 0 && index < MAX_CAPACITY && index < self.queue.len() {
+            self.queue.insert(index, commit);
+        } else {
+            self.queue.push(commit);
+        }
+
+        // emit event element was added
+    }
+
+    pub fn dequeue(&mut self) -> Option<Commit> {
+        let index = self.pointer;
+        self.pointer += 1;
+        return self.queue[index];
+    }
+
+    pub fn is_valid_insert(&self, commit: Commit) -> bool {
+        return self.queue.len() == MAX_CAPACITY
+            && commit.amount > self.queue[self.queue.len() - 1];
+    }
+
+    pub fn is_valid_dequeue(&self) -> bool {
+        return self.point < MAX_CAPACITY;
+    }
+
+    pub fn is_valid_session(&self, session: Session) -> bool {
+        return self.session == session.key();
+    }
+}
+
+pub struct Commit {
+    pub bidder_index: u32,
+    pub amount: u64,
 }
 
 pub enum Status {
@@ -46,5 +220,6 @@ impl Status {
 
 // sealed bid system
 //  SealedBidRound
+//  SealedBidByIndex
 //  CommitQueue
 //  CommitLeaderBoard
