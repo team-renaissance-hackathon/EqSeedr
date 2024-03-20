1384aa4dba44810274f024e4edef82592246b0b5
nomadic4life <orbison.dennis@gmail.com>
2024-03-19 19:09:48 -0700

    will have to go in the route of creating a linked list for each type that will use it. IDL generation seesm it doesn't work for generic types. will explore IDL generics in a future date. EnqueueSessionIndex and ActiveSessionIndex are partially implemented.

--- programs/launch_pad/src/states/indexer_status.rs
+++ programs/launch_pad/src/states/indexer_status.rs
@@ -3,11 +3,59 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct IndexerStatus {
+    // VALIDATION STATE
     pub bump: u8,
     pub authority: Pubkey,
+
+    // STATE
     pub status: Indexer,
 }
 
 impl IndexerStatus {
     pub const LEN: usize = DISCRIMINATOR + BUMP + PUBKEY_BYTES + Indexer::LEN;
+
+    pub fn initialize(&self) {}
 }
+
+#[account]
+pub struct EnqueueSessionIndex {
+    // VALIDATION STATE
+    pub bump: u8,
+    pub authority: Pubkey,
+    // STATE
+    pub list: Vec<Indexer>,
+    pub stack: Vec<u16>,
+}
+
+#[account]
+pub struct ActiveSessionIndex {
+    // VALIDATION STATE
+    pub bump: u8,
+    pub authority: Pubkey,
+    // STATE
+    pub list: Vec<Indexer>,
+    pub stack: Vec<u16>,
+}
+
+// #[derive(AnchorDeserialize, AnchorSerialize, Clone)]
+// pub struct IndexerLinkedList {
+//     pub total: u32,
+//     head: u32,
+//     tail: u32,
+//     list: Vec<Node>,
+
+//     stack: Vec<[u8; 3]>,
+// }
+
+// #[derive(AnchorDeserialize, AnchorSerialize, Clone)]
+// pub struct Node {
+//     pub next: u8,
+//     pub prev: u8,
+//     pub position: Indexer,
+// }
+
+// currently EnqueueSessionIndex and ActiveSessionIndex are being created, but they are not fully
+// implemented that handles the list data
+// adding to list, removing from list, and tracking empty elements in the list
+// that implementation detail is not important right now. will just use the IndexerStatus
+// to track sessions
--- programs/launch_pad/src/states/session_indexer.rs
+++ programs/launch_pad/src/states/session_indexer.rs
@@ -6,12 +6,17 @@ pub struct SessionIndexer {
     pub bump: u8,
     pub authority: Pubkey,
 
-    // not sure this will work in anchor
-    pub list: Vec<(Indexer, Pubkey)>,
+    pub list: Vec<Index>,
 }
 
 impl SessionIndexer {
     const LIMIT: usize = 100;
     pub const LEN: usize =
         DISCRIMINATOR + BUMP + PUBKEY_BYTES + (Indexer::LEN + PUBKEY_BYTES) * SessionIndexer::LIMIT;
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize, Clone)]
+pub struct Index {
+    pub indexer: Indexer,
+    pub session: Pubkey,
 }
