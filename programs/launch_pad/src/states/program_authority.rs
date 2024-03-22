d1ada1fd7d2def75bc18ac99a46e3f05026523d0
nomadic4life <orbison.dennis@gmail.com>
2024-03-21 22:36:08 -0700

    implemented create marketplace matchers with client help and tests. also there seems to be a bug with anchor initializing with bumps when intilizing lots of state contracts. will look into it later.

--- programs/launch_pad/src/states/program_authority.rs
+++ programs/launch_pad/src/states/program_authority.rs
@@ -7,10 +7,17 @@ pub struct ProgramAuthority {
 
     // specify a authority or deployer?
     // pub authority: Pubkey,
-    pub is_initialzied: bool,
+    pub is_initialized: bool,
     pub is_signer: bool,
 }
 
 impl ProgramAuthority {
     pub const LEN: usize = DISCRIMINATOR + BUMP + BOOL + BOOL;
+
+    pub fn initialize(&mut self, bump: u8) {
+        self.is_initialized = true;
+        self.is_signer = true;
+
+        self.bump = bump;
+    }
 }
